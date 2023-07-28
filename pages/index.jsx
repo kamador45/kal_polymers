e7af0974e0aa62d98b361eaa60e975117a767de0
Kamador <kamador@ocfnyc.com>
2023-07-28 01:31:04 -0600

    kal-polymers website

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+      <h1>SS</h1>
+  )
+}
+
+
+export default Home;
